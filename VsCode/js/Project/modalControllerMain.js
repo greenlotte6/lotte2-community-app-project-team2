756398098506ebcf3612744bdac024cbd318cb3e
jinwoooooooo <jinwooya55@gmail.com>
2025-06-04 19:20:51 +0900

    프로젝트 페이지 수정

--- VsCode/js/Project/modalControllerMain.js
+++ VsCode/js/Project/modalControllerMain.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded', function () {
     const modalContainer = document.querySelector(".modalContainer");
+    const modalWrapper = document.querySelector(".modalWrapper");
     const addProject = document.querySelector(".addProject");
     const addProjectModal = document.querySelector(".addProjectModal");
     const modalClose = document.querySelectorAll(".modalClose");
@@ -8,6 +9,7 @@ document.addEventListener('DOMContentLoaded', function () {
     addProject.addEventListener("click", function (e) {
         e.stopPropagation(); // 이벤트가 document까지 가지 않게 막음
         modalContainer.style.display = "flex";
+        modalWrapper.style.display = "block";
         document.body.style.overflow = "hidden";
     });
 
@@ -16,6 +18,7 @@ document.addEventListener('DOMContentLoaded', function () {
         // 클릭된 요소가 모달(.addProjectModal) 안에 없다면 닫기
         if (!addProjectModal.contains(e.target)) {
             modalContainer.style.display = "none";
+            modalWrapper.style.display = "none";
             document.body.style.overflow = "visible";
         }
     });
@@ -28,6 +31,7 @@ document.addEventListener('DOMContentLoaded', function () {
     modalClose.forEach((e) => {
         e.addEventListener("click", function() {
             modalContainer.style.display = "none";
+            modalWrapper.style.display = "none";
             document.body.style.overflow = "visible";
         })
     });
