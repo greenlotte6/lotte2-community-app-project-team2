7952836edcbe5edd254c95db73bc4bdf33b85ed0
sjo112777 <sjo112777@gmail.com>
2025-06-11 17:11:53 +0900

    배포 테스트

--- React/src/api/_http.js
+++ React/src/api/_http.js
@@ -1,6 +1,8 @@
 //const SERVER_HOST = "http://localhost:8080";
 export const SERVER_HOST =
-  import.meta.env.MODE === "development" ? "http://localhost:8080" : "/api"; // 배포 환경에서는 /api 경로를 사용 (Vercel 프록시)
+  import.meta.env.MODE === "development"
+    ? "http://localhost:8080"
+    : "http://54.180.113.128:8080"; // 또는 배포용 도메인 (https://api.linkon.site)
 
 // user
 export const USER_TERMS = `${SERVER_HOST}/terms`;
